1af228fe6ef0e809e862550f02a179e059083242
Ashutosh Chouhan <123181968+Ashutoshac11@users.noreply.github.com>
2024-06-17 17:37:14 +0530

    Update settings.py

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -13,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-@0$r)g-f_qyu6_rp*f&s_k34^ur03epuhoyr%jq$m$k*08vo^^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "") != "False"
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
